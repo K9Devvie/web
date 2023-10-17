d12a7fd47fe2ab8fedb09b2c21804818c4496b46
@K9Devvie <corazonjada210@gmail.com>
2023-10-17 23:13:55 +0800

    first commit

--- pages/Rooms.tsx
+++ pages/Rooms.tsx
@@ -19,7 +19,7 @@ export default function Sell() {
                      <Heading style={{ color: "Cyan", fontFamily: 'Concert One'  }} >Enter A Chat Room</Heading>
             <Text style={{ color: "Cyan", fontFamily: 'Concert One'  }} >Please purchase a Scrollpass to enter a specific room.</Text>
             <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
-            <Link as={NextLink} href="Anon's_Room_1" mx={5} style={{ fontStyle: 'normal', color: 'Cyan', fontFamily: 'Concert One' }} >
+            <Link as={NextLink} href="Anons_Room_1" mx={5} style={{ fontStyle: 'normal', color: 'Cyan', fontFamily: 'Concert One' }} >
                 <Flex alignItems="center">
                 <Image src="https://media.licdn.com/dms/image/D4E0BAQF6gMSNL5xYCA/company-logo_200_200/0/1692892036736/scroll_io_logo?e=2147483647&v=beta&t=lN1d9ETN4PIIvDN4cWIaKzhzIp8DJMt3WxkOru6OBMA" alt="Room Image" boxSize={10} ml={3} borderRadius="full" />
                     <Text fontWeight="lighter" color="Cyan" ml={2}>Anon&apos;s Room 1</Text> {/* Make text orange */}
@@ -48,7 +48,7 @@ export default function Sell() {
             <Link as={NextLink} href="Anons_Room_1" mx={5} style={{ fontStyle: 'normal', color: 'Cyan', fontFamily: 'Concert One' }} >
                 <Flex alignItems="center">
                 <Image src="https://media.licdn.com/dms/image/D4E0BAQF6gMSNL5xYCA/company-logo_200_200/0/1692892036736/scroll_io_logo?e=2147483647&v=beta&t=lN1d9ETN4PIIvDN4cWIaKzhzIp8DJMt3WxkOru6OBMA" alt="Room Image" boxSize={10} ml={3} borderRadius="full" />
-                <Text fontWeight="lighter" color="Cyan" ml={2}>Anon&apos;s Room</Text>
+                <Text fontWeight="lighter" color="Cyan" ml={2}>Anon&apos;s Room 2</Text>
 
 
                 </Flex>
